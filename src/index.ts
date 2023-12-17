b15d128b7320bd600ac53571437c98d116ed6712
cheapshot003 <dercheapi@protonmail.com>
2023-12-17 17:45:46 +0100

    success message

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import helmet from "helmet";
 const path = require('path');
 import { Database } from "bun:sqlite";
-
+const fs = require("fs");
 
 function getlastid(db_name : string): number {
     var id: number;
@@ -28,7 +28,7 @@ if (!process.env.PORT) {
 }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
-
+const ROOT_PATH: string = (process.env.ROOT_PATH as string);
 const app = express();
 
 app.use(helmet());
@@ -36,15 +36,28 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
+
+
+async function getFile(filepath: string): Promise<string> {
+    var file: string = await Bun.file(ROOT_PATH + filepath).text();
+    return file;
+}
+
+
 app.get('/success/:id', (req, res) => {
-    res.send(`NICE! ID: ${req.params.id}`)
+    var id = req.params.id;
+    res.writeHead(200, { "Content-Type": "text/html" });
+    getFile("/src/success.html").then((file) => {
+        
+        res.end(file.replace("$USER", "Ole"));
+    })
 })
 app.post('/submit', (req, res) => {
     const db = new Database("database.db");
     var id: number = getlastid("database.db") + 1;
     db.run(`INSERT INTO users (id, user, npub) VALUES (?,?,?)`, [id, req.body.username, req.body.npub]);
     console.log(`PUSHED TO DB: ID: ${id}, USERNAME: ${req.body.username}, NPUB: ${req.body.npub}`);
-    res.redirect(`http://localhost:7000/success/${id}`)
+    res.redirect(`http://localhost:7000/success/${id}`);
     db.close()
 })
 app.listen(PORT, () => {
